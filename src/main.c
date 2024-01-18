8a6e7286c4fbd1140c5c49b13de8c083b27a6cbd
mathieu-MZ <mathieu.mazeau@epitech.eu>
2024-01-18 15:41:23 +0100

    FIX

--- src/main.c
+++ src/main.c
@@ -31,6 +31,16 @@ typedef struct {
     float y;
 } Platform;
 
+typedef struct {
+    float x;
+    float y;
+} HealthSprite;  // Nouvelle structure pour le sprite de santé
+
+typedef enum {
+    MENU,
+    GAME
+} GameState;
+
 void updateDino(Dino *dino, float deltaTime) {
     if (dino->isJumping) {
         dino->y += dino->velocity * deltaTime;
@@ -60,13 +70,13 @@ sfBool checkGroundCollision(Dino *dino) {
 Platform createRandomPlatform() {
     Platform platform;
     platform.x = WINDOW_WIDTH;
-    platform.y = WINDOW_HEIGHT - GROUND_HEIGHT - (rand() % 100 + 60);
+    platform.y = WINDOW_HEIGHT - GROUND_HEIGHT - (rand() % 130 + 80);
     return platform;
 }
 
 Cactus createRandomCactus() {
     Cactus cactus;
-    cactus.x = WINDOW_WIDTH;
+    cactus.x = WINDOW_WIDTH + (rand() % 400 + 200);
     cactus.y = WINDOW_HEIGHT - GROUND_HEIGHT - CACTUS_SIZE;
     return cactus;
 }
@@ -80,7 +90,19 @@ void drawPlatform(sfRenderWindow *window, Platform *platform) {
     sfRectangleShape_destroy(platformShape);
 }
 
-void drawGame(sfRenderWindow *window, Dino *dino, Platform *platform, Cactus *cactus, int score, sfSprite *backgroundSprite) {
+void drawHealth(sfRenderWindow *window, HealthSprite *healthSprite) {
+    sfRectangleShape *healthShape = sfRectangleShape_create();
+    sfSprite *healthSprite2 = sfSprite_create();
+    sfTexture *healthTexture = sfTexture_createFromFile("asset/PV.png", NULL);
+    sfSprite_setTexture(healthSprite2, healthTexture, sfTrue);
+    sfSprite_setPosition(healthSprite2, (sfVector2f){WINDOW_WIDTH - 120, 10});
+    sfRenderWindow_drawSprite(window, healthSprite2, NULL);
+    sfSprite_destroy(healthSprite2);
+    sfTexture_destroy(healthTexture);
+    sfRectangleShape_destroy(healthShape);
+}
+
+void drawGame(sfRenderWindow *window, Dino *dino, Platform *platform, Cactus *cactus, int score, sfSprite *backgroundSprite, HealthSprite *healthSprite) {
     sfRenderWindow_clear(window, sfBlack);
 
     // Move the background
@@ -115,7 +137,7 @@ void drawGame(sfRenderWindow *window, Dino *dino, Platform *platform, Cactus *ca
     sfSprite *cactusSprite = sfSprite_create();
     sfTexture *cactusTexture = sfTexture_createFromFile("asset/spike.png", NULL);
     sfSprite_setTexture(cactusSprite, cactusTexture, sfTrue);
-    sfSprite_setPosition(cactusSprite, (sfVector2f){cactus->x, cactus->y+10});
+    sfSprite_setPosition(cactusSprite, (sfVector2f){cactus->x, cactus->y-35});
     sfRenderWindow_drawSprite(window, cactusSprite, NULL);
     sfSprite_destroy(cactusSprite);
     sfTexture_destroy(cactusTexture);
@@ -133,25 +155,45 @@ void drawGame(sfRenderWindow *window, Dino *dino, Platform *platform, Cactus *ca
     sfRenderWindow_drawText(window, scoreText, NULL);
     sfText_destroy(scoreText);
 
+    // Draw the health sprite
+    drawHealth(window, healthSprite);
+
     sfRenderWindow_display(window);
     sfFont_destroy(font);
 }
 
-void handleEvents(sfRenderWindow *window, Dino *dino) {
+void drawMenu(sfRenderWindow *window, sfSprite *menuSprite) {
+    sfRenderWindow_clear(window, sfBlack);
+
+    // Draw the menu background
+    sfRenderWindow_drawSprite(window, menuSprite, NULL);
+
+    sfRenderWindow_display(window);
+}
+
+void handleEvents(sfRenderWindow *window, Dino *dino, GameState *gameState) {
     sfEvent event;
     while (sfRenderWindow_pollEvent(window, &event)) {
         if (event.type == sfEvtClosed) {
             sfRenderWindow_close(window);
         }
 
-        if (event.type == sfEvtKeyPressed && event.key.code == sfKeySpace && !dino->isJumping) {
+        if (*gameState == MENU && event.type == sfEvtMouseButtonPressed) {
+            // Check if the mouse click is on the menu image
+            sfVector2i mousePos = sfMouse_getPositionRenderWindow(window);
+            if (mousePos.x >= 100 && mousePos.x <= 400 && mousePos.y >= 300 && mousePos.y <= 500) {
+                *gameState = GAME;
+            }
+        }
+
+        if (*gameState == GAME && event.type == sfEvtKeyPressed && event.key.code == sfKeySpace && !dino->isJumping) {
             dino->isJumping = sfTrue;
             dino->velocity = -600.0f;
         }
     }
 }
 
-void initializeGame(sfRenderWindow **window, Dino *dino, Platform *platform, Cactus *cactus, sfClock **clock, sfSprite **backgroundSprite) {
+void initializeGame(sfRenderWindow **window, Dino *dino, Platform *platform, Cactus *cactus, sfClock **clock, sfSprite **backgroundSprite, sfSprite **menuSprite, HealthSprite *healthSprite) {
     sfVideoMode mode = {WINDOW_WIDTH, WINDOW_HEIGHT, 32};
     *window = sfRenderWindow_create(mode, "Dino Game", sfResize | sfClose, NULL);
 
@@ -166,6 +208,11 @@ void initializeGame(sfRenderWindow **window, Dino *dino, Platform *platform, Cac
     *backgroundSprite = sfSprite_create();
     sfSprite_setTexture(*backgroundSprite, backgroundTexture, sfTrue);
 
+    // Load the menu texture and sprite
+    sfTexture *menuTexture = sfTexture_createFromFile("asset/Page.png", NULL);
+    *menuSprite = sfSprite_create();
+    sfSprite_setTexture(*menuSprite, menuTexture, sfTrue);
+
     dino->x = 100.0f;
     dino->y = WINDOW_HEIGHT - GROUND_HEIGHT - DINO_SIZE;
     dino->velocity = 0.0f;
@@ -173,7 +220,9 @@ void initializeGame(sfRenderWindow **window, Dino *dino, Platform *platform, Cac
     dino->isOnPlatform = sfFalse;
 
     *platform = createRandomPlatform();
-    cactus->x = WINDOW_WIDTH;
+    *cactus = createRandomCactus();
+
+    *healthSprite = (HealthSprite){WINDOW_WIDTH - 60, 10};
 
     *clock = sfClock_create();
 }
@@ -185,59 +234,68 @@ int main() {
     Cactus cactus;
     sfClock *clock;
     sfSprite *backgroundSprite;
+    sfSprite *menuSprite;
+    GameState gameState = MENU;
+    HealthSprite healthSprite;
 
     int score = 0;
     float scoreTimer = 0.0f;
 
-    initializeGame(&window, &dino, &platform, &cactus, &clock, &backgroundSprite);
+    initializeGame(&window, &dino, &platform, &cactus, &clock, &backgroundSprite, &menuSprite, &healthSprite);
 
     while (sfRenderWindow_isOpen(window)) {
-        handleEvents(window, &dino);
+        handleEvents(window, &dino, &gameState);
 
         sfTime deltaTime = sfClock_restart(clock);
-        updateDino(&dino, sfTime_asSeconds(deltaTime));
 
-        // Check collision with the ground
-        if (checkGroundCollision(&dino)) {
-            dino.y = WINDOW_HEIGHT - GROUND_HEIGHT - DINO_SIZE;
-            dino.isJumping = sfFalse;
-            dino.isOnPlatform = sfFalse;
+        if (gameState == GAME) {
+            updateDino(&dino, sfTime_asSeconds(deltaTime));
+
+            // Check collision with the ground
+            if (checkGroundCollision(&dino)) {
+                dino.y = WINDOW_HEIGHT - GROUND_HEIGHT - DINO_SIZE;
+                dino.isJumping = sfFalse;
+                dino.isOnPlatform = sfFalse;
+            }
+
+            // Check collision with the platform
+            if (checkCollision(&dino, &platform) && dino.y < platform.y) {
+                dino.y = platform.y - DINO_SIZE;
+                dino.isJumping = sfFalse;
+                dino.isOnPlatform = sfTrue;
+            } else {
+                dino.isOnPlatform = sfFalse;
+            }
+
+            // Update the score every 0.1 seconds
+            scoreTimer += sfTime_asSeconds(deltaTime);
+            if (scoreTimer >= 0.1f) {
+                score++;
+                scoreTimer = 0.0f;
+            }
+
+            if (cactus.x < 0) {
+                cactus = createRandomCactus();
+            } else {
+                cactus.x -= 300.0f * sfTime_asSeconds(deltaTime);
+            }
+
+            if (platform.x + PLATFORM_WIDTH < 0) {
+                platform = createRandomPlatform();
+            } else {
+                platform.x -= 300.0f * sfTime_asSeconds(deltaTime);
+            }
+
+            drawGame(window, &dino, &platform, &cactus, score, backgroundSprite, &healthSprite);
+        } else if (gameState == MENU) {
+            drawMenu(window, menuSprite);
         }
-
-        // Check collision with the platform
-        if (checkCollision(&dino, &platform) && dino.y < platform.y) {
-            dino.y = platform.y - DINO_SIZE;
-            dino.isJumping = sfFalse;
-            dino.isOnPlatform = sfTrue;
-        } else {
-            dino.isOnPlatform = sfFalse;
-        }
-
-        // Update the score every 0.1 seconds
-        scoreTimer += sfTime_asSeconds(deltaTime);
-        if (scoreTimer >= 0.1f) {
-            score++;
-            scoreTimer = 0.0f;
-        }
-
-        if (cactus.x < 0) {
-            cactus = createRandomCactus();
-        } else {
-            cactus.x -= 300.0f * sfTime_asSeconds(deltaTime);
-        }
-
-        if (platform.x + PLATFORM_WIDTH < 0) {
-            platform = createRandomPlatform();
-        } else {
-            platform.x -= 300.0f * sfTime_asSeconds(deltaTime);
-        }
-
-        drawGame(window, &dino, &platform, &cactus, score, backgroundSprite);
     }
 
     sfRenderWindow_destroy(window);
     sfClock_destroy(clock);
     sfSprite_destroy(backgroundSprite);
+    sfSprite_destroy(menuSprite);
 
     return EXIT_SUCCESS;
 }
